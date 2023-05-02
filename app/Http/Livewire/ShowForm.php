5ef0b77383f36a9f4aad68ee3d4d3cd9fd01baa4
Andrea Lanciano <andrealanciano@me.com>
2023-05-02 16:44:24 +0200

    finito il form con regole e messaggi personalizzati

--- app/Http/Livewire/ShowForm.php
+++ app/Http/Livewire/ShowForm.php
@@ -14,15 +14,37 @@ class ShowForm extends Component
     public $year;
     public $genre;
 
+
+    protected $rules = [
+        'title' => 'required|min:10|max:200',
+        'subtitle' => 'required|min:10|max:255',
+        'author' => 'required|min:10',
+        'year' => 'required|min:4',
+        'genre' => 'required',
+        'abstract' => 'required|min:200',
+    ];
+
+
+    protected $messages = [
+        'title.required' => 'Il titolo è di importanza rilevante!',
+        'subtitle.required' => 'Il sotto titolo è di importanza rilevante!',
+        'author.required' => "L'autore è di importanza rilevante!",
+        'year.required' => "l'anno è di importanza rilevante",
+        'year.min' => "l'anno va inserito secondo questo formato: YYYY",
+        'genre.required' => 'Il genere è di importanza rilevante!',
+        'abstract.required' => 'La trama è di importanza rilevante!',
+    ];
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
     public function store(){
-        Article::create([
-            'title' => $this->title,
-            'subtitle' => $this->subtitle,
-            'abstract' => $this->abstract,
-            'author' => $this->author,
-            'year' => $this->year,
-            'genre' => $this->genre,
-        ]);
+
+        $validatedForm = $this->validate();
+
+        Article::create($validatedForm);
 
         $this->reset();
     }
--- resources/views/livewire/show-form.blade.php
+++ resources/views/livewire/show-form.blade.php
@@ -8,27 +8,33 @@
                 <div class="mb-3">
                     <label for="title" class="form-label">Title</label>
                     <input type="text" class="form-control" wire:model="title">
+                    @error('title') <span class="error text-danger">{{ $message }}</span> @enderror
                 </div>
                 <div class="mb-3">
                     <label for="subtitle" class="form-label">Subtitle</label>
                     <input type="text" class="form-control" wire:model="subtitle">
+                    @error('subtitle') <span class="error text-danger">{{ $message }}</span> @enderror
                 </div>
                 <label for="abstract" class="form-label">Plot</label>
                 <div class="mb-3">
                     <textarea class="form-control" cols="30" rows="10" wire:model="abstract"></textarea>
                 </div>
+                @error('abstract') <span class="error text-danger">{{ $message }}</span> @enderror
                 <div class="mb-3">
                     <label for="author" class="form-label">Author</label>
                     <input type="text" class="form-control" wire:model="author">
+                    @error('author') <span class="error text-danger">{{ $message }}</span> @enderror
                 </div>
                 <div class="d-flex justify-content-between">
                     <div class="mb-3 w-50 me-3">
                         <label for="year" class="form-label">Year</label>
                         <input type="number" class="form-control" wire:model="year">
+                        @error('year') <span class="error text-danger">{{ $message }}</span> @enderror
                     </div>
                     <div class="mb-3 w-50 ms-3">
                         <label for="genre" class="form-label">Genre</label>
                         <input type="text" class="form-control" wire:model="genre">
+                        @error('genre') <span class="error text-danger">{{ $message }}</span> @enderror
                     </div>
                 </div>
                 <button type="submit" class="btn btn-primary">Save</button>
